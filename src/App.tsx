4fbfb6fb855ff7b3e903d97523252aec92d8e27c
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-12-10 16:47:48 +0300

    fix: rebuild project settings 10 дек 2023 г. 16:47:48

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <main>
+      {/* <Route path="/" element={<MainPage />}>
+        <Route path="frontpage=:page&details=:id" element={<DetailBlock />} />
+      </Route>
+      <Route path="/page=:pageNumber" element={<MainPage />} />
+      <Route path="/*" element={<ErrorPage />} /> */}
+    </main>
+  )
+);
+
+const App = (): JSX.Element => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/controllers/appControllers.tsx
@@ -0,0 +1,1 @@
+import { useState, ReactNode, useMemo } from 'react';
