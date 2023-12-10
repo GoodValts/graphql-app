fa03343123b0667ccf9c8b386f95b754a8cae096
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-12-10 16:53:30 +0300

    fix: husky prepush 10 дек 2023 г. 16:53:30

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,6 @@ const router = createBrowserRouter(
       {/* <Route path="/" element={<MainPage />}>
         <Route path="frontpage=:page&details=:id" element={<DetailBlock />} />
       </Route>
-
-
-
-
-
-
       <Route path="/page=:pageNumber" element={<MainPage />} />
       <Route path="/*" element={<ErrorPage />} /> */}
     </main>
