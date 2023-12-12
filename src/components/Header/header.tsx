54a2e60dc25f6a9855d47f7a60cb79bee985389c
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-12-12 10:48:40 +0300

    feat: add animation of header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,15 +1,34 @@
 import { Nav, Stack, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import styles from './header.module.scss';
 import logo from '../../assets/img/logo.png';
 import { AuthContext } from '../../controllers/appControllers';
 
+let timeout;
+let scroll = 0;
+
 const Header = (): JSX.Element => {
   const { isAuth } = useContext(AuthContext);
+  const [scrollWindow, setScrollWindow] = useState(false);
   const navigate = useNavigate();
+  useEffect(() => {
+    window.onscroll = (): void => {
+      scroll = window.scrollY;
+      if (scroll >= window.scrollY && window.scrollY > 10) {
+        console.log(window.scrollY);
+        setScrollWindow(true);
+      } else {
+        setScrollWindow(false);
+      }
+    };
+  }, []);
   return (
-    <header className={styles.header}>
+    <header
+      className={
+        scrollWindow ? `${styles.header} ${styles.sticky}` : styles.header
+      }
+    >
       <Stack direction="horizontal" gap={4}>
         <img src={logo} className={styles.logo} alt="logo" />
         <span>PhoenixGraphiQL</span>
