35297236870d26f006c4925ef43c3e41c7d11602
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-12-22 10:18:59 +0300

    fix: correct input component[201~

--- src/components/Forms/Input/input.tsx
+++ src/components/Forms/Input/input.tsx
@@ -2,9 +2,7 @@ import { FieldError } from 'react-hook-form/dist/types';
 import { FieldValues } from 'react-hook-form/dist/types/fields';
 import { UseFormRegister } from 'react-hook-form/dist/types/form';
 import { Path } from 'react-hook-form/dist/types/path';
-import { Dispatch, SetStateAction, useRef, useState } from 'react';
-import visibleImg from './inputAssets/visible-icon.png';
-import unVisibleImg from './inputAssets/unVisible-icon.png';
+import { Dispatch, SetStateAction } from 'react';
 import defStyles from './input.module.scss';
 
 type InputProps<FormValues extends FieldValues> = {
@@ -28,65 +26,25 @@ const Input = <FormValues extends FieldValues>({
   register,
   required,
   errors,
-  setState,
 }: InputProps<FormValues>): JSX.Element => {
   let errorText = '';
   if (errors && errors.message) {
     errorText = errors.message;
   }
 
-  const [isVisible, setIsVisible] = useState(false);
-  const formInput = useRef<HTMLInputElement>(null);
-
-  const showHidePassword = (img: HTMLImageElement): void => {
-    const input = formInput.current;
-    if (isVisible) {
-      setIsVisible(false);
-      img.src = unVisibleImg;
-      if (input) input.type = 'password';
-    } else {
-      setIsVisible(true);
-      img.src = visibleImg;
-      if (input) input.type = 'text';
-    }
-  };
-
   return (
     <>
       <label htmlFor={name as string} className={defStyles.label}>
         {label}
       </label>
-      <div className={defStyles.container}>
-        <input
-          id={name as string}
-          accept={accept}
-          type={type}
-          className={`${defStyles.input} ${styles}`}
-          autoComplete={name as string}
-          {...register(name as Path<FormValues>, { required })}
-          onChange={(e): void => {
-            if (setState) setState(e.target.value);
-          }}
-          // ref={formInput}
-        />
-        {type === 'password' && (
-          <div className={defStyles.imgContainer}>
-            <button
-              className={defStyles.imgButton}
-              type="button"
-              onClick={(e): void =>
-                showHidePassword(e.target as HTMLImageElement)
-              }
-            >
-              <img
-                alt="show/hide"
-                src={unVisibleImg}
-                className={defStyles.img}
-              />
-            </button>
-          </div>
-        )}
-      </div>
+      <input
+        id={name as string}
+        accept={accept}
+        type={type}
+        className={`${defStyles.input} ${styles}`}
+        autoComplete={name as string}
+        {...register(name as Path<FormValues>, { required })}
+      />
       <p className={defStyles.errorMessage}>{errorText}</p>
     </>
   );
