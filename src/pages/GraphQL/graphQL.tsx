62a6d7915338d68cecee71cd598bcdb4325f4561
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-12-24 01:18:31 +0300

    feat: implement requests

--- src/pages/GraphQL/graphQL.tsx
+++ src/pages/GraphQL/graphQL.tsx
@@ -1,26 +1,78 @@
+import { useContext, useState } from 'react';
 import styles from './graphQL.module.scss';
+import { AuthContext } from '../../controllers/appControllers';
+import GrapgQLtextObj from './langData';
+
+interface ResponseData {
+  data: undefined;
+}
 
 const GraphQLPage = (): JSX.Element => {
+  const [url, setUrl] = useState('https://rickandmortyapi.com/graphql');
+  const [query, setQuery] = useState(`query {
+    characters {
+      results {
+        name
+        status
+      }
+    }
+  }`);
+  const [response, setResponse] = useState('');
+  const { lang } = useContext(AuthContext);
+
+  const makeRequest = async (): Promise<ResponseData> => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+    return res.json();
+  };
+
+  const printData = (): void => {
+    makeRequest().then((res): void => {
+      setResponse(JSON.stringify(res.data, undefined, 2));
+    });
+  };
+
   return (
     <div className={styles.wrapper}>
       <form className={styles.header}>
-        <button className={styles.button} type='button'>Prettify</button>
+        <button className={styles.button} type="button">
+          {GrapgQLtextObj[lang].prettify}
+        </button>
         <input
           className={styles.url_input}
           type="url"
-          placeholder="Enter URL"
+          placeholder={GrapgQLtextObj[lang].urlPlaceholder}
+          value={url}
+          onChange={(e): void => setUrl(e.target.value)}
         />
-        <button className={styles.button} type='submit'>Submit</button>
+        <button className={styles.button} type="submit">
+          {GrapgQLtextObj[lang].submit}
+        </button>
       </form>
-      <div className={styles.code_wrapper}>
-        <div className={styles.code}>
+      <div className={styles.query_wrapper}>
+        <div className={styles.query}>
           <textarea
-            className={styles.code_input}
-            placeholder="Input code here"
+            className={styles.query_input}
+            placeholder={GrapgQLtextObj[lang].queryPlaceholder}
+            value={query}
+            onChange={(e): void => setQuery(e.target.value)}
           />
-          <button className={styles.button_run} type='button'>Run</button>
+          <button
+            className={styles.button_run}
+            type="button"
+            onClick={(): void => printData()}
+          >
+            <div className={styles.arrow_right} />
+          </button>
         </div>
-        <div className={styles.response} />
+        <div className={styles.response}>
+          <pre>{response}</pre>
+        </div>
       </div>
     </div>
   );
