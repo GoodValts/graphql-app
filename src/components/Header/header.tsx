6de17fcd3b07fa988b5cf2d897e7be431918765a
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-12-26 01:38:51 +0300

    feat: add header styles 26 дек 2023 г.  1:38:51

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,6 +2,7 @@ import { Nav, Stack, Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { key } from 'localforage';
 import logo from '../../assets/img/logo3.png';
 import { auth, logout } from '../../firebase/firebase';
 import { AuthContext } from '../../controllers/appControllers';
@@ -9,7 +10,7 @@ import styles from './header.module.scss';
 
 let scroll = 0;
 
-const radios = [
+const radios: { name: string; value: string }[] = [
   { name: 'En', value: 'en' },
   { name: 'Рус', value: 'ru' },
 ];
@@ -40,9 +41,6 @@ const Header = (): JSX.Element => {
   const navigate = useNavigate();
 
   const { lang, setLang } = useContext(AuthContext);
-  const [radioValue, setRadioValue] = useState(
-    localStorage.getItem('lang') || 'en'
-  );
   const [scrollWindow, setScrollWindow] = useState(false);
   const [user] = useAuthState(auth);
 
@@ -63,92 +61,114 @@ const Header = (): JSX.Element => {
         scrollWindow ? `${styles.header} ${styles.sticky}` : styles.header
       }
     >
-      <Stack direction="horizontal" gap={4}>
-        <a className={styles.logoContainer} href="/">
-          <img src={logo} className={styles.logo} alt="logo" />
-          <p className={styles.logoText}>{textObj[lang].project}</p>
-        </a>
-      </Stack>
-      <Stack
-        direction="horizontal"
-        gap={2}
-        className={`ms-auto ${styles.container}`}
-      >
-        <ButtonGroup>
-          {radios.map((radio, ind) => (
-            <ToggleButton
+      <a className={styles.logoContainer} href="/">
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p className={styles.logoText}>{textObj[lang].project}</p>
+      </a>
+
+      <div className={styles.headerPanel}>
+        <div className={styles.languageContainer}>
+          {radios.map((radio, id) => (
+            <button
               key={radio.value}
-              id={`radio-${ind}`}
-              type="radio"
-              name="radio"
-              variant="outline-light"
-              value={radio.value}
-              size="lg"
-              checked={radioValue === radio.value}
-              data-testid={radio.value}
-              onChange={(e): void => {
-                setRadioValue(e.currentTarget.value);
-                const selectedLang = radios
-                  .find((item) => item.value === e.currentTarget.value)!
-                  .value.toLowerCase();
-                setLang(selectedLang);
-                localStorage.setItem('lang', selectedLang);
+              type="button"
+              className={`
+              ${styles.toggleButton}
+              ${id === 0 ? styles.first : ''}
+              ${id === radios.length - 1 ? styles.last : ''}
+              ${radio.value === lang ? styles.selected : ''}
+            `
+                .replace(/\n/g, '')
+                .replace(/ {2,}/g, ' ')
+                .trim()}
+              onClick={(): void => {
+                setLang(radio.value);
+                localStorage.setItem('lang', radio.value);
               }}
-              className={styles.red}
             >
               {radio.name}
-            </ToggleButton>
+            </button>
           ))}
-        </ButtonGroup>
-        {user ? (
-          <>
-            <Nav
-              className={styles.nav}
-              fill
-              variant="pills"
-              defaultActiveKey="/"
-            >
-              <Nav.Item>
-                <Nav.Link
-                  // href="/graphQL"
-                  eventKey="graphQL"
-                  data-testid="graphql-btn"
-                  onClick={(): void => navigate('/graphQL')}
-                >
-                  GraphQL
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Button
-              variant="outline-info"
-              size="lg"
+        </div>
+        <nav>
+          {user ? (
+            <button
+              type="button"
+              data-testid="graphql-btn"
+              className={styles.navButton}
               onClick={(): void => {
                 logout();
                 navigate('/');
               }}
             >
               {textObj[lang].signOut}
-            </Button>
-          </>
-        ) : (
-          <Stack direction="horizontal" gap={2} className="ms-auto">
-            <Button
-              variant="outline-info"
-              size="lg"
-              onClick={(): void => navigate('/login')}
-            >
-              {textObj[lang].signIn}
-            </Button>
-            <Button
-              variant="outline-info"
-              size="lg"
-              onClick={(): void => navigate('/registration')}
-            >
-              {textObj[lang].signUp}
-            </Button>
-          </Stack>
-        )}
-      </Stack>
+            </button>
+          ) : (
+            // <>
+            //   <Nav
+            //     className={styles.nav}
+            //     fill
+            //     variant="pills"
+            //     defaultActiveKey="/"
+            //   >
+            //     <Nav.Item>
+            //       <Nav.Link
+            //         // href="/graphQL"
+            //         eventKey="graphQL"
+            //         data-testid="graphql-btn"
+            //         onClick={(): void => navigate('/graphQL')}
+            //       >
+            //         GraphQL
+            //       </Nav.Link>
+            //     </Nav.Item>
+            //   </Nav>
+            //   <Button
+            //     variant="outline-info"
+            //     size="lg"
+            //     onClick={(): void => {
+            //       logout();
+            //       navigate('/');
+            //     }}
+            //   >
+            //     {textObj[lang].signOut}
+            //   </Button>
+            // </>
+
+            // <Stack direction="horizontal" gap={2} className="ms-auto">
+            //   <Button
+            //     variant="outline-info"
+            //     size="lg"
+            //     onClick={(): void => navigate('/login')}
+            //   >
+            //     {textObj[lang].signIn}
+            //   </Button>
+            //   <Button
+            //     variant="outline-info"
+            //     size="lg"
+            //     onClick={(): void => navigate('/registration')}
+            //   >
+            //     {textObj[lang].signUp}
+            //   </Button>
+            // </Stack>
+            <div className={styles.nav}>
+              <button
+                type="button"
+                className={styles.navButton}
+                onClick={(): void => navigate('/login')}
+              >
+                {textObj[lang].signIn}
+              </button>
+              <button
+                type="button"
+                className={styles.navButton}
+                onClick={(): void => navigate('/registration')}
+              >
+                {textObj[lang].signUp}
+              </button>
+            </div>
+          )}
+        </nav>
+      </div>
     </header>
   );
 };
