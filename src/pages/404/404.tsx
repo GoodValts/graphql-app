c256060ccbb84bafcfd1ff9468c2ae4ff365bac8
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-01-07 14:02:27 +0300

    feat: add localization  7 янв 2024 г. 14:02:27

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,8 +1,28 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../controllers/appControllers';
 import styles from './404.module.scss';
+
+const textObj: {
+  [key: string]: {
+    message: string;
+    button: string;
+  };
+} = {
+  en: {
+    message: 'Page not found',
+    button: 'Return to main page',
+  },
+  ru: {
+    message: 'Страница не найдена',
+    button: 'На главную',
+  },
+};
 
 const NotFoundPage = (): JSX.Element => {
   const navTo = useNavigate();
+
+  const { lang } = useContext(AuthContext);
 
   const backToMain = (): void => {
     navTo('/');
@@ -11,9 +31,9 @@ const NotFoundPage = (): JSX.Element => {
   return (
     <section className={styles.page}>
       <div className={styles.errorBlock}>
-        <div className={styles.text}>Page not found!</div>
+        <div className={styles.text}>{textObj[lang].message}!</div>
         <button type="button" onClick={backToMain} className={styles.button}>
-          Return to main page
+          {textObj[lang].button}
         </button>
       </div>
     </section>
